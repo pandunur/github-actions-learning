ec9a9c4ff20aa791704d7399339e0bb08f0997b8
pandunur <pandunurhisyam12@gmail.com>
2026-09-15 10:39:25 +0700

    ci: pytest
    Signed-off-by: pandunur <pandunurhisyam12@gmail.com>

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print("Application started")
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
+    print("2 * 3 =", multiply(2, 3))
