009b0c9f78a9bb8d7dcddb46edf10b19bc2f6404
pandunur <pandunurhisyam12@gmail.com>
2026-09-16 14:26:07 +0700

    app.py changing

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
     print("Application started")
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
-    print("2 * 3 =", multiply(2, 3))\n
+    print("2 * 3 =", multiply(2, 3))
+
